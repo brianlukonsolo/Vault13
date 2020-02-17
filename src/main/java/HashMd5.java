e9caa66302d2e117272cb3bd7d93e0f779d31422
Brian Lukonsolo <brian.lukonsolo@gmail.com>
2020-02-17 21:18:30 +0000

    refactor(*): Rename variables in initial md5 function

--- src/main/java/HashMd5.java
+++ src/main/java/HashMd5.java
@@ -7,10 +7,10 @@ public class HashMd5 {
     public String calculateMd5DigestValue(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         MessageDigest md5Digest = MessageDigest.getInstance("md5");
         md5Digest.update(text.getBytes());
-        byte[] dgst = md5Digest.digest();
+        byte[] digestAsBytes = md5Digest.digest();
         StringBuilder byteToString = new StringBuilder();
-        for(Byte c : dgst){
-            byteToString.append(String.format("%02x", c));
+        for(Byte singleByte : digestAsBytes){
+            byteToString.append(String.format("%02x", singleByte));
         }
 
         return byteToString.toString();
